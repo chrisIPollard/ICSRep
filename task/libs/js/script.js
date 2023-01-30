867c9c8c18aed906e60dce51247f6dd22961a937
chrisIPollard <112253442+chrisIPollard@users.noreply.github.com>
2023-01-30 14:50:57 +0000

    Update script.js

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -1,12 +1,44 @@
-	$('#btnRun').click(function() {
+	document.getElementById('btnRun1').onclick = function btnfunc1(){
+
+	$.ajax({
+		url: "libs/php/getAirportInfo.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			ICAO: $('#selAirport').val()
+			
+		},
+		success: function(result) {
+
+			console.log(JSON.stringify(result));
+
+			if (result.status.name == "ok") {
+
+				$('#txtClouds').html(result['data']['clouds']);
+				$('#txtTemperature').html(result['data']['temperature']);
+				$('#txtHumidity').html(result['data']['humidity']);
+				
+
+			}
+		
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+		}
+	}); 
+
+};	
+
+
+document.getElementById('btnRun2').onclick = function btnfunc2(){
 
 		$.ajax({
-			url: "libs/php/getAirportInfo.php",
+			url: "libs/php/getLLWeatherInfo.php",
 			type: 'POST',
 			dataType: 'json',
 			data: {
-				ICAO: $('#selAirport').val()
-				
+				lat: $('#selLat').val(),
+				lng: $('#selLng').val()
 			},
 			success: function(result) {
 
@@ -14,12 +46,11 @@
 
 				if (result.status.name == "ok") {
 
-					$('#txtClouds').html(result['data'][0]['clouds']);
-					$('#txtTemperature').html(result['data'][0]['temperature']);
-					$('#txtHumidity').html(result['data'][0]['humidity']);
-					$('#txtWindDirection').html(result['data'][0]['windDirection']);
-					$('#txtWindSpeed').html(result['data'][0]['windSpeed']);
-
+					$('#txtClouds').html(result['data']['clouds']);
+					$('#txtTemperature').html(result['data']['temperature']);
+					$('#txtHumidity').html(result['data']['humidity']);
+					
+					
 				}
 			
 			},
@@ -28,6 +59,33 @@
 			}
 		}); 
 	
-	});
+	};
 
+	document.getElementById('btnRun3').onclick = function btnfunc3(){
+
+		$.ajax({
+			url: "libs/php/getNeighbourInfo.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				geonameId: $('#selCountry').val()
+				
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#txtNeighbour').html(result['data'][1]['countryName']);
+										
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR);
+			}
+		}); 
+	
+	};
 	
