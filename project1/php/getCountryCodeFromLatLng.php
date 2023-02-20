cb46baceb37bad1062966ed90ec4d1f573cdf7f6
chrisIPollard <112253442+chrisIPollard@users.noreply.github.com>
2023-02-20 19:54:33 +0000

    Update getCountryCodeFromLatLng.php

--- project1/php/getCountryCodeFromLatLng.php
+++ project1/php/getCountryCodeFromLatLng.php
@@ -1,6 +1,6 @@
 <?php
 
-	//this curl contacts the geonames API with lat & lng as a request then returns the JSON file to the script which includes the country name and code. 
+	//this curl contacts the geonames API with lat & lng as a request then returns the country code. 
 
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
@@ -17,17 +17,18 @@
 	$result=curl_exec($ch);
 
 	curl_close($ch);
-
+	echo $result;
 	$decode = json_decode($result,true);	
 
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['codes'];
-	
+	$output['countryCode'] = $decode['countryCode'];
+		
 	header('Content-Type: application/json; charset=UTF-8');
 
 	echo json_encode($output); 
 
 ?>
+
