04bde89dd1fd5bab357ecccf58fa0695bbd201f3
chrisIPollard <112253442+chrisIPollard@users.noreply.github.com>
2023-02-20 19:53:22 +0000

    Update script.js

--- project1/js/script.js
+++ project1/js/script.js
@@ -3,9 +3,9 @@
 let latitude;
 let longitude;
 let coordinates;
+let countryNamesAndCodes;
 let select = document.getElementById("selCountry");
-
-
+let localCountryCode;
 
 //2. these are placeholder coordinates so I can tell at a glance if the functions are working:
 
@@ -26,61 +26,85 @@ var map = L.map('map').setView([startLat, startLng], 6);
                       
                       
 
-// //4. Geolocation (change/fix the event!):
+//4. This runs Geolocation and returns a country code value for id='txtCountryCode':
 
-// document.getElementById('placeholder').onclick = function geoFindMe(coordinates) {
+$(function geoFindMe(coordinates) {
   
-// 	// API call as a function: 
+	// API call as a function: 
 
-// function APILatLngCall(position){
+function APILatLngCall(position){
 
-// 	console.log(position.coords.latitude);
+	console.log(position.coords.latitude);
 
-// 	$.ajax({
-// 		url: "php/getCountryCodeFromLatLng.php",
-// 		type: 'POST',
-// 		dataType: 'json',
-// 		data: {
-// 			lat: position.coords.latitude,
-// 			lng: position.coords.longitude
-// 		},
-// 		success: function(result) {
+	$.ajax({
+		url: "php/getCountryCodeFromLatLng.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		},
+		success: function(result) {
 
-// 			console.log(JSON.stringify(result));
+			console.log(JSON.stringify(result));
 
-// 			if (result.status.name == "ok") {
+			if (result.status.name == "ok") {
+        localCountryCode = result['countryCode'];
+        console.log(localCountryCode);
 
-// 				$('#txtCountryCode').html(result['data']['code']);
-								
-// 			}
+
+				$('#txtCountryCode').value(localCountryCode);
+
+        //sending the code to the local file to get country coordinates:
+				// $(function getCountryCoordinates(){
+        // console.log(result['data']['code']);
+
+        // $.ajax({
+        //   url: 'php/getCoordinatesFromLocalFile.php',
+        //   type: 'GET',
+        //   dataType: "json",
+        //   data: {countryCode: result['data']['code']},
+
+        //   success: function(result) {
+  
+        //   coordinates = result.result;
+        //   var borders = L.geoJSON(coordinates).addTo(map);
+        //   map.fitBounds(borders.getBounds());},
+
+        //   error: function(jqXHR, textStatus, errorThrown) {
+        //     console.log(jqXHR);
+        //   }
+        // }); 
+        // })				
+			}
 		
-// 		},
-// 		error: function(jqXHR, textStatus, errorThrown) {
-// 			console.log(jqXHR);
-// 		}
-// 	}); 
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+		}
+	}); 
 
-// };
+};
   
-// 	function error() {
-// 	  console.log('Unable to retrieve your location');
-// 	}
+	function error() {
+	  console.log('Unable to retrieve your location');
+	}
 
 // this is the bit that runs the geolocation method using the API function from above:
 
-// 	if (!navigator.geolocation) {
-// 		console.log('Geolocation is not supported by your browser');
-// 	} else {
-// 		console.log('Locating');
-// 	  navigator.geolocation.getCurrentPosition(APILatLngCall, error);
-// 	}
+	if (!navigator.geolocation) {
+		console.log('Geolocation is not supported by your browser');
+	} else {
+		console.log('Locating');
+	  navigator.geolocation.getCurrentPosition(APILatLngCall, error);
+	}
   
-// }
+})
 
-//5. This is an Ajax request to a local GeoJSON file via a PHP file. It returns country coordinates to leaflet and views the country, however it should be changed to run at the start. 
+//5. This is an Ajax request to a local GeoJSON file via a PHP file. It returns country coordinates to leaflet and views the country. 
 
-//document.getElementById('btnRun1').onclick = function getCountryCoordinates(){
-        //console.log($('#selCountry').val());
+document.getElementById('btnRun1').onclick = function getCountryCoordinates(){
+        console.log($('#selCountry').val());
 
         $.ajax({
           url: 'php/getCoordinatesFromLocalFile.php',
@@ -102,7 +126,7 @@ var map = L.map('map').setView([startLat, startLng], 6);
             console.log(jqXHR);
           }
         }); 
-   // }
+    }
 
 //6 This is an is an Ajax request to a local GeoJSON file via a PHP file. It returns country names and codes in alphabetical pairs to populate the drop down as the JSON object 'countryCodes'.  
 
@@ -115,19 +139,22 @@ $(function getCountryCodes(){
           
           success: function(countryCodes) {
 
-          console.log(countryCodes);
-
           
-          // console.log(countryCodes.countryCodes.length);
-          //   $(function populateDropdown() {
-          //   for (let i = 0; i < countryCodes.countryCodes.length; i++) {
-          //   var optn = countryCodes.countryCodes[i];
-          //   var opt = document.createElement("option");
-          //   opt.textContent = i[0];
-          //   opt.value = i[1];
-          //   select.appendChild(opt);
-          //   }})
-            },
+          countryNamesAndCodes = countryCodes.countryCodes;
+          //console.log(countryNamesAndCodes);
+          countryNamesAndCodes = JSON.parse(countryNamesAndCodes);
+          //console.log(countryNamesAndCodes);
+          //console.log(typeof(countryNamesAndCodes));
+          //console.log(countryNamesAndCodes.length);
+              $(function populateDropdown() {
+              for (let i = 0; i < countryNamesAndCodes.length; i++) {
+              var optn = countryNamesAndCodes[i];
+              var opt = document.createElement("option");
+              opt.textContent = optn[0];
+              opt.value = optn[1];
+              select.appendChild(opt);
+              }})
+          },
        
           error: function(jqXHR, textStatus, errorThrown) {
             console.log(jqXHR);
