de20232dc230fb13a5d990963f1ee6d5e08f7212
chrisIPollard <112253442+chrisIPollard@users.noreply.github.com>
2023-03-02 11:15:27 +0000

    Add files via upload

---
+++ project1/js/script.js
@@ -0,0 +1,96 @@
+document.getElementById('btnRun1').onclick = function btnfunc1(){
+
+	$.ajax({
+		url: "libs/php/getAirportInfo.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			ICAO: $('#selAirport').val()
+			
+		},
+		success: function(result) {
+
+			console.log(JSON.stringify(result));
+
+			if (result.status.name == "ok") {
+
+				$('#txtClouds').html(result['data']['clouds']);
+				$('#txtTemperature').html(result['data']['temperature']);
+				$('#txtHumidity').html(result['data']['humidity']);
+				
+
+			}
+		
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+		}
+	}); 
+
+};	
+
+
+document.getElementById('btnRun2').onclick = function btnfunc2(){
+
+		$.ajax({
+			url: "libs/php/getLLWeatherInfo.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				lat: $('#selLat').val(),
+				lng: $('#selLng').val()
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#txtClouds').html(result['data']['clouds']);
+					$('#txtTemperature').html(result['data']['temperature']);
+					$('#txtHumidity').html(result['data']['humidity']);
+					
+					
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR);
+			}
+		}); 
+	
+	};
+
+	document.getElementById('btnRun3').onclick = function btnfunc3(){
+
+		$.ajax({
+			url: "libs/php/getNeighbourInfo.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				geonameId: $('#selCountry').val()
+				
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#txtNeighbour').html(result['data'][1]['countryName']);
+										
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR);
+			}
+		}); 
+	
+	};
+
+	 
+	
+
+
+	
