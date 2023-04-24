a4f1a868518fcecf1a0f75f5dfa83e036a49aebb
ChrPollard <112253442+ChrPollard@users.noreply.github.com>
2023-04-24 11:56:09 +0100

    populate table

--- project2/js/script.js
+++ project2/js/script.js
@@ -35,7 +35,28 @@ $(function getTableData(){
 	  success: function(result) {
 	  
 	  console.log(result);
+		console.log(result.data);
+		let data = result.data;
 	  
+	  console.log ('array length '+ data.length);
+	  for (let n = 0; n < data.length; n ++){
+		$('#tabledata').append(`
+		<td>
+			<span class="custom-checkbox">
+				<input type="checkbox" id="checkbox${n}" name="options[]" value="1">
+					<label for="checkbox${n}"></label>
+			</span>
+		</td>
+		<td>${data[n].firstName} + ' ' + ${data[n].lastName}</td>
+		<td>${data[n].email}</td>
+		<td>${data[n].department}</td>
+		<td>${data[n].location}</td>
+		<td>
+			<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+			<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+		</td>
+		`);
+		}
 	  },
    
 	  error: function(jqXHR, textStatus, errorThrown) {
